47c18fd13a720fc5dbce3cf13b8dfab33a5b207b
William Barbosa <william.sb@hotmail.com>
2017-06-26 22:07:24 -0300

    :x: Initial work on operator DSL

--- MvvmCross.FSharp/BindingOperators.fs
+++ MvvmCross.FSharp/BindingOperators.fs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace MvvmCross.FSharp
+ 
+open MvvmCross.FSharp.BindingContext
+
+module BindingOperators = 
+
+    let (|>>) set childTarget =
+        set |> bind childTarget
+
+    let (>>) targetPropertyName description =
+        targetPropertyName |> toTarget description
+
+    let (>>>>) targetPropertyName description =
+        targetPropertyName |> forProperty description        
