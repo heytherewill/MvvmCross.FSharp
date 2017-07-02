90f1c43867bc0da2114f4d5d8a2f3660a41a7485
William Barbosa <william.sb@hotmail.com>
2017-07-02 12:32:49 -0300

    :muscle: Allow quotations in bindings

--- MvvmCross.FSharp/BindingContext.fs
+++ MvvmCross.FSharp/BindingContext.fs
@@ -1,8 +1,10 @@
 ﻿namespace MvvmCross.FSharp
 
+open Microsoft.FSharp.Quotations.Patterns
+open MvvmCross.Binding
 open MvvmCross.Binding.BindingContext
 
-module BindingContext = 
+module BindingContext =
     let bind
         childTarget
         (set: MvxFluentBindingDescriptionSet<'TOwningTarget, 'TSource>) =
@@ -12,11 +14,33 @@ module BindingContext =
         (targetPropertyName: string)
         (description: MvxFluentBindingDescription<'TTarget, 'TSource>) =
         description.For(targetPropertyName)
-   
-    let toTarget 
+
+    let forExpression<'TTarget, 'TSource, 'T
+        when 'TTarget : not struct
+        and 'TSource : not struct
+        and 'T : not struct>
+        (quotation: Quotations.Expr)
+        (description: MvxFluentBindingDescription<'TTarget, 'TSource>) =
+        
+        match quotation with
+        | PropertyGet(_, propertyInfo, _) -> description.For(propertyInfo.Name)
+        | _ -> failwith "Invalid property expression"
+
+    let toTarget
         (targetPropertyName: string)
         (description: MvxFluentBindingDescription<'TTarget, 'TSource>)  =
         description.To(targetPropertyName)
+
+    let toExpression<'TTarget, 'TSource, 'T
+        when 'TTarget : not struct
+        and 'TSource : not struct
+        and 'T : not struct>
+        (quotation: Quotations.Expr)
+        (description: MvxFluentBindingDescription<'TTarget, 'TSource>) =
+        
+        match quotation with
+        | PropertyGet(_, propertyInfo, _) -> description.To(propertyInfo.Name)
+        | _ -> failwith "Invalid property expression"
 
     let apply (set: MvxFluentBindingDescriptionSet<'TOwningTarget, 'TSource>) =
         set.Apply()
