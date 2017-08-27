2910d12084767be7a3547efe144a7020fce02bd5
William Barbosa <william.sb@hotmail.com>
2017-08-27 11:39:02 -0300

    :package: Update build script

--- build.fsx
+++ build.fsx
@@ -3,15 +3,30 @@ open Fake
  
 let buildDir = "./bin/"
 let targetSln = ["./MvvmCross.FSharp.sln"]
-let pclFile = "./bin/Release/MvvmCross.FSharp.dll"
-let pclDestination = "./build/lib/portable-net45+win+wpa81+wp80/"
+
+let coreDll = "./bin/Release/MvvmCross.FSharp.dll"
+let iOSDll = "./bin/Release/MvvmCross.FSharp.iOS.dll"
+let droidDll = "./bin/Release/MvvmCross.FSharp.Droid.dll"
+
+let copyOperations = [
+  ("./build/lib/portable-net45+win+wpa81+wp80/", [ coreDll ])
+  ("./build/lib/Xamarin.iOS10/", [ coreDll ; iOSDll ])
+  ("./build/lib/MonoAndroid10/", [ coreDll ; droidDll ])
+]
  
 // Targets
 Target "Clean" (fun _ -> CleanDir buildDir)
 Target "Build" (fun _ -> MSBuildRelease "" "" targetSln |> Log "Build: ")
-Target "Copy" (fun _ -> 
-  CreateDir pclDestination
-  CopyFile pclDestination pclFile
+Target "Copy" (fun _ ->
+  copyOperations
+  |> Seq.iter (fun tuple ->
+    let destination = fst tuple 
+    CreateDir destination
+    let copyFn = CopyFile destination
+    tuple
+    |> snd
+    |> Seq.iter copyFn
+  )
 )
 
 // Dependencies
